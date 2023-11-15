2218910d4a4ecb6ed556ac3309367f62d3fe9ada
Georgi <gogolino1982@gmail.com>
2023-11-15 19:44:47 +0200

    add server

--- Workshop_main/client/src/App.jsx
+++ Workshop_main/client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
 }
 
 export default App;
--- Workshop_main/readMe.md
+++ Workshop_main/readMe.md
@@ -1,2 +1,5 @@
 Make folder client & server
 install React in client folder (npm create vite . -- --template react)
+open server and npm i to get the newest packages
+npm run build and copy the server.js file from dist folder
+run server ( node .\server.js)
