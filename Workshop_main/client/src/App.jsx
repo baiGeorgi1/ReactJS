37341e9f0ec552f76186d62bfbcef63757663114
Georgi <gogolino1982@gmail.com>
2023-11-28 12:07:06 +0200

    some fixes

--- Workshop_main/client/src/App.jsx
+++ Workshop_main/client/src/App.jsx
@@ -1,9 +1,7 @@
 // Подреди импортите по значение
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import * as authService from "./services/authService";
-import AuthContext, { AuthProvider } from "./contexts/auth";
+import { AuthProvider } from "./contexts/auth";
 
 import CatalogPage from "./components/catalogPage/CatalogPage";
 import Header from "./components/header/Header";
@@ -14,57 +12,26 @@ import GameDetails from "./components/gameDetails/GameDetails";
 import Path from "./paths";
 
 function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem("accessToken");
-    return {};
-  });
-
-  const loginHandler = async (values) => {
-    const result = await authService.login(values.email, values.password);
-    setAuth(result);
-    localStorage.setItem("accessToken", result.accessToken);
-    navigate(Path.Home);
-  };
-
-  const registerHandler = async (values) => {
-    const result = await authService.register(values.email, values.password);
-    setAuth(result);
-    localStorage.setItem("accessToken", result.accessToken);
-    navigate(Path.Home);
-  };
-  const logoutHandler = () => {
-    setAuth({});
-    navigate(Path.Home);
-    localStorage.removeItem("accessToken");
-  };
-
-  const info = {
-    loginHandler,
-    registerHandler,
-    logoutHandler,
-    username: auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken, //ако имаме юзър ще върне true
-  };
-
-  return (
-    // use Auth.Provider for context
-    // на value можем да подаваме ст-сти,обекти,ф-ии,хендлъри и т.н.
-    <AuthProvider value={info}>
-      <div id="box">
-        <Header />
-        <Routes>
-          <Route path={Path.Home} element={<Home />} />
-          <Route path="/users/*" element={<UserPath />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          {/* <Route path="/logout" element={<Logout />} /> */}
-          <Route path="/catalog/:gameId/details" element={<GameDetails />} />
-          <Route path="/create" element={<CreatePage />} />
-        </Routes>
-      </div>
-    </AuthProvider>
-  );
+    return (
+        // use Auth.Provider for context
+        // на value можем да подаваме ст-сти,обекти,ф-ии,хендлъри и т.н.
+        <AuthProvider>
+            <div id="box">
+                <Header />
+                <Routes>
+                    <Route path={Path.Home} element={<Home />} />
+                    <Route path="/users/*" element={<UserPath />} />
+                    <Route path="/catalog" element={<CatalogPage />} />
+                    {/* <Route path="/logout" element={<Logout />} /> */}
+                    <Route
+                        path="/catalog/:gameId/details"
+                        element={<GameDetails />}
+                    />
+                    <Route path="/create" element={<CreatePage />} />
+                </Routes>
+            </div>
+        </AuthProvider>
+    );
 }
 
 export default App;
--- Workshop_main/client/src/contexts/auth.jsx
+++ Workshop_main/client/src/contexts/auth.jsx
@@ -1,6 +1,7 @@
-/** @format */
-
-import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import * as authService from "../services/authService";
+import { useState, createContext } from "react";
+import Path from "../paths";
 
 const AuthContext = createContext();
 
@@ -8,14 +9,44 @@ const AuthContext = createContext();
 
 AuthContext.displayName = "AuthContext";
 
-export const AuthProvider = ({
-    children,
+export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem("accessToken");
+        return {};
+    });
 
-    value,
-}) => {
-    return (
-        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-    );
+    const loginHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+        setAuth(result);
+        localStorage.setItem("accessToken", result.accessToken);
+        navigate(Path.Home);
+    };
+
+    const registerHandler = async (values) => {
+        const result = await authService.register(
+            values.email,
+            values.password,
+        );
+        setAuth(result);
+        localStorage.setItem("accessToken", result.accessToken);
+        navigate(Path.Home);
+    };
+    const logoutHandler = () => {
+        setAuth({});
+        navigate(Path.Home);
+        localStorage.removeItem("accessToken");
+    };
+
+    const info = {
+        loginHandler,
+        registerHandler,
+        logoutHandler,
+        username: auth.email,
+        email: auth.email,
+        isAuthenticated: !!auth.accessToken, //ако имаме юзър ще върне true
+    };
+    return <AuthContext.Provider value={info}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
